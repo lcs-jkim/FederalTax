e0f0e509799a401ac71b72e5d88fdadea41975c5
Amanda <jkim@lcs.on.ca>
2019-11-01 11:07:25 -0400

    Connected all the outlets and starting switch statement

--- FederalTax/ViewController.swift
+++ FederalTax/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK PROPERTIES
+    @IBOutlet weak var Name: UITextField!
+    @IBOutlet weak var TotalIncome: UITextField!
+    @IBOutlet weak var TotalTaxDue: UILabel!
+    @IBOutlet weak var EffectiveTaxRate: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
+    @IBAction func CalculateTax(_ sender: Any) {
+        
+        guard Name.text != nil else {
+            return
+        }
+        guard let TotalIncomeAsDouble = TotalIncome.text else {
+            return
+        }
+        
+        switch TotalTaxDue.text {
+        case <#pattern#>:
+            <#code#>
+        default:
+            <#code#>
+        }
+    
+    }
 
 }
 
